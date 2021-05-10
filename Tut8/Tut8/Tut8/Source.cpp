e009a1baccca5a819a28acf450efc43a2c7212cc
William Breece <kbd1114@gmail.com>
2021-05-09 19:29:12 -0500

    Struct initialization and structs containing other structs.

--- Tut8/Tut8/Tut8/Source.cpp
+++ Tut8/Tut8/Tut8/Source.cpp
@@ -2,15 +2,32 @@
 #include <string>
 using namespace std;
 
+struct LocationVector
+{
+	float X;
+	float Y;
+	float Z;
+};
+
 struct Character
 {
 	string Name;
 	float Health;
 	int Level;
 	float Damage;
+
+	LocationVector Location = { 0.f, 0.f, 0.f };
+
 	void Attack()
 	{
 		cout << Name << " does " << Damage << " damage!" << endl;
+	}
+
+	void Displaylocation()
+	{
+		cout << "X: " << Location.X << endl;
+		cout << "Y: " << Location.Y << endl;
+		cout << "Z: " << Location.X << endl;
 	}
 };
 
@@ -23,15 +40,18 @@ int main()
 	falmir.Damage = 2.5f;
 
 	falmir.Attack();
+	falmir.Displaylocation();
 
 	Character brenwyn = {
 		"Brenwyn",
 		100.f,
 		3,
-		15.f
+		15.f,
+		{35.5f, 67.45f, 100.003f}
 	};
 
 	brenwyn.Attack();
+	brenwyn.Displaylocation();
 
 	system("pause");
 }
